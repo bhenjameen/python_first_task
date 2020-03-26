18817cca88a5f8fe8430ea8936020cad16816402
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-03-26 18:54:47 +0100

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,4 @@
+print('Welcome to my circle-area calculator')
+from math import pi
+r = float(input ("Kindly input the radius of your circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
